d6ebb8b0b27550a63c4b79b8c16002215bb45cbf
Heljä Lehtinen <lehthelj@gmail.com>
2023-03-27 22:16:21 +0300

    unicafe tehtävän testejä, vaatimusmäärittelyn, gitignoren ja työaikakirjanpidon lisäys

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,31 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_saldo_kasvaa_ladatessa_oikein(self):
+        self.maksukortti.lataa_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 15.00 euroa")
+
+    def test_saldo_vahenee_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahaa_riittavasti_on_True(self):
+        totuusarvo = self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(totuusarvo, True)
+
+    def test_rahaa_ei_riittavasti_on_False(self):
+        totuusarvo1 = self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(totuusarvo1, False)
